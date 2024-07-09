a6ae49c73816bcdddb52c011774833ca8ba0e510
Akhil Pallem <126126352+AkhilPallem@users.noreply.github.com>
2024-07-09 18:40:28 -0500

    Update MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -8,13 +8,13 @@ public class Solution {
             }
             count += (num == candidate) ? 1 : -1;
         }
-        return candidate; // Returning an Integer as double (implicit conversion)
+        return candidate; // Returning an Integer as double 
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        // Example usage:
+        // Example
         int[] nums = {2, 2, 1, 1, 1, 2, 2};
         double majorityElement = solution.majorityElement(nums); // Storing the result in a double
         System.out.println("Majority element: " + majorityElement);
@@ -22,5 +22,4 @@ public class Solution {
 }
 
     // Given an array nums of size n, return the majority element.
-
     // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
